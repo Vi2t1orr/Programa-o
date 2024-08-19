49336aa8ddec74896fc81fd82636bcb52982a342
Vi2t1orr <141885532+Vi2t1orr@users.noreply.github.com>
2024-08-19 09:01:26 -0300

    Update index.php

--- trabalho/index.php
+++ trabalho/index.php
@@ -1,38 +1,66 @@
 <?php require_once('inc/topo.php');
 session_start();
+error_reporting(0);
 $_SESSION['product-name'] = 'Placa de Vídeo Asus Dual NVIDIA GeForce RTX 2070 EVO V2 OC Edition, 8GB, GDDR6';
-$_SESSION['product-price'] = 2949.90;
+
 
 if (!isset($_SESSION['qtde_pedido_item'])) {
    $_SESSION['qtde_pedido_item'] = 1;
+   $_SESSION['product-price'] = 2949.90;
+   $_SESSION['cupom_uso'] = 0;
 }
+
+$cupom1 = 'desc10';
+$cupom2 = 'desc20';
 
 // Verifica se o botão de incremento ou decremento foi clicado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+   if (isset($_POST['cupom'])) {
+     
+      if($_SESSION['cupom_uso'] === 0){
+         if($_POST['cupom'] === $cupom1 ){
+            $_SESSION['product-price'] = $_SESSION['product-price'] *0.9;
+            $_SESSION['cupom_uso'] = 1;
+         }
+         if($_POST['cupom'] === $cupom2 ){
+            $_SESSION['product-price'] = $_SESSION['product-price'] *0.8;
+            $_SESSION['cupom_uso'] = 1;
+         }
+      }
+   }
    if (isset($_POST['incrementar'])) {
       
       $_SESSION['qtde_pedido_item']++; // Incrementa o contador
+      $_SESSION['product-total'] = $_SESSION['product-price'] * $_SESSION['qtde_pedido_item'];
       
    } elseif (isset($_POST['decrementar'])) {
+
+
       if( $_SESSION['qtde_pedido_item'] > 1){
+
          $_SESSION['qtde_pedido_item']--; // Decrementa o contador
+         $_SESSION['product-total'] = $_SESSION['product-total'] - $_SESSION['product-price'];
+         
       }
-      
    }
 }
 
+
+
 if(isset( $_REQUEST['acao'] ) && $_REQUEST['acao'] == 'delete'){
-   unset($_SESSION);
+   unset($_SESSION['product-price']);
+   unset($_SESSION['qtde_pedido_item']);
 
    $_SESSION['delete'] = false;
+   $_SESSION['cupom_uso'] = 0;
 
-}else{
+} else{
+
    $_SESSION['delete'] = true;
+
 }
-$_SESSION['product-subtotal'] =  $_SESSION['product-price'] *  $_SESSION['qtde_pedido_item'];
-if (isset($_SESSION['product-remove'])){
-   $_SESSION['qtde_pedido_item'] = 1;
-}
+
 ?>
      
       <div class="main_content">
@@ -48,6 +76,7 @@ if (isset($_SESSION['product-remove'])){
                                  <th class="product-name">Produto</th>
                                  <th class="product-price">Preço</th>
                                  <th class="product-quantity">Quantidade</th>
+                                 <th class="product-total">Total</th>
                                  <th class="product-subtotal">Subtotal</th>
                                  <th class="product-remove">Remove</th>
                               </tr>
@@ -57,7 +86,7 @@ if (isset($_SESSION['product-remove'])){
                               <tr>
                                  <td class="product-thumbnail"><a href=""><img src="http://localhost/ifc/trabalho/img/produto.jpg" alt="Placa de Vídeo Asus Dual NVIDIA GeForce RTX 2070 EVO V2 OC Edition, 8GB, GDDR6"></a></td>
                                  <td class="product-name" data-title="Product"><a href=""><?=$_SESSION['product-name']?></a></td>
-                                 <td class="product-price" data-title="Price">R$ <?php echo $_SESSION['product-price']?></td>
+                                 <td class="product-total" data-title="Total">R$ <?= $_SESSION['product-price']?></td>
                                  <td class="product-quantity" data-title="Quantity">
                                     <div class="quantity">
                                        <form method="post" action="" class="text-center mt-4">
@@ -67,7 +96,7 @@ if (isset($_SESSION['product-remove'])){
                                        </form>
                                     </div>
                                  </td>
-                                 <td class="product-subtotal" data-title="Total">R<a href=""><?=$_SESSION['product-name']?></a></td>
+                                 <td class="product-subtotal" data-title="Total">R$ <?= ($_SESSION['qtde_pedido_item'] * $_SESSION['product-price'])?></td>
                                  <td class="product-remove qtde" tipo="remove" id_produto="5" data-title="Remove"><a href="?acao=delete">X</a></td>
                               </tr>
                               <?php } ?>
@@ -78,10 +107,12 @@ if (isset($_SESSION['product-remove'])){
                                     <div class="row no-gutters align-items-center">
                                        <div class="col-lg-12 col-md-12 mb-12 mb-md-12">
                                           <div class="coupon field_form input-group">
-                                             <input type="text" value="" class="form-control form-control-sm" placeholder="Código do cupom">
-                                             <div class="input-group-append">
-                                                <button class="btn btn-fill-out btn-sm" type="submit">Aplicar cupom</button>
-                                             </div>
+                                             <form method="post" action="" class="text-center mt-4">
+                                                <input type="text" value="" name="cupom" class="form-control form-control-sm" placeholder="Código do cupom">
+                                                <div class="input-group-append">
+                                                   <button class="btn btn-fill-out btn-sm" type="submit">Aplicar cupom</button>
+                                                </div>
+                                          </form>
                                           </div>
                                        </div>
                                     </div>
